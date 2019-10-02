25d43f69b1d23123e7e5927bb18ffa8bfdffca74
Juan Sebastian Gaviria Prada <jsgp_1994@hotmail.com>
2019-10-01 21:13:03 -0500

    Se carga proyecto de ejemplo para enviar correos

---
+++ enviar_email/resources/views/company/index.blade.php
@@ -0,0 +1,5 @@
+@extends('layouts.app')
+
+@section('content')
+    <p>Se envio el correo con exito</p>
+@endsection
---
+++ enviar_email/resources/views/mail/base.blade.php
@@ -0,0 +1,5 @@
+@extends('layouts.app')
+
+@section('content')
+    <h1>Este es el primer correo</h1>
+@endsection
